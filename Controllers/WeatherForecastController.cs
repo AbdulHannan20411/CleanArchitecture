ea6399aa1e19e4022444802ca745fa7dc55153f0
Abdul Hannan <abdulhannan204115@gmail.com>
2024-11-05 19:23:28 +0500

    .

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ToDoApplication.Controllers
 {
     [ApiController]
-    [Route("/cat")]
+    [Route("/weather")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
