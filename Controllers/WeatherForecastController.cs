db043a3b0e872476b2b0f66a9a428d87c4c57562
AbdulHannan20411 <133662303+AbdulHannan20411@users.noreply.github.com>
2024-11-05 19:54:48 +0500

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ToDoApplication.Controllers
 {
     [ApiController]
-    [Route("/notHere")]
+    [Route("/notHereaa")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
